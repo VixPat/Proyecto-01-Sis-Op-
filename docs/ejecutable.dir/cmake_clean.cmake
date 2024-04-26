453379f6c704711a65094471ec8f0c6ca225f23c
VixPat <168156823+VixPat@users.noreply.github.com>
2024-04-25 23:32:53 -0500

    Add files via upload

---
+++ docs/ejecutable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejecutable.dir/oscilador.c.o"
+  "CMakeFiles/ejecutable.dir/oscilador.c.o.d"
+  "ejecutable"
+  "ejecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ejecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
